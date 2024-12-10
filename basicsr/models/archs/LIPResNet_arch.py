0b41ec04d28e2839e7e0a44c8f5ff50bc7e1d7c1
ycwsilent <3496107969@qq.com>
2024-12-10 17:19:22 +0800

    Update LIPResNet_arch.py

--- basicsr/models/archs/LIPResNet_arch.py
+++ basicsr/models/archs/LIPResNet_arch.py
@@ -1,1 +1,518 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from einops import rearrange
+import math
+import warnings
+from torch.nn.init import _calculate_fan_in_and_fan_out
+from pdb import set_trace as stx
+
+class ChannelAttention(nn.Module):
+
+    def __init__(self, input_channels, internal_neurons):
+        super(ChannelAttention, self).__init__()
+        self.fc1 = nn.Conv2d(in_channels=input_channels, out_channels=internal_neurons, kernel_size=1, stride=1,
+                             bias=True)
+        self.fc2 = nn.Conv2d(in_channels=internal_neurons, out_channels=input_channels, kernel_size=1, stride=1,
+                             bias=True)
+        self.input_channels = input_channels
+
+
+    def forward(self, inputs, ill):
+        x1 = F.adaptive_avg_pool2d(inputs, output_size=(1, 1))
+        x1 = self.fc1(x1)
+        x1 = F.relu(x1, inplace=True)
+        x1 = self.fc2(x1)
+        x1 = torch.sigmoid(x1)
+
+        x2 = F.adaptive_max_pool2d(inputs, output_size=(1, 1))
+        x2 = self.fc1(x2)
+        x2 = F.relu(x2, inplace=True)
+        x2 = self.fc2(x2)
+        x2 = torch.sigmoid(x2)
+        x = x1 + x2
+        x3 = F.adaptive_max_pool2d(ill, output_size=(1, 1))
+        x3 = self.fc1(x3)
+        x3 = F.relu(x3, inplace=True)
+        x3 = self.fc2(x3)
+        x3 = torch.sigmoid(x3)
+
+        x4 = F.adaptive_avg_pool2d(ill, output_size=(1, 1))
+        x4 = self.fc1(x4)
+        x4 = F.relu(x4, inplace=True)
+        x4 = self.fc2(x4)
+        x4 = torch.sigmoid(x4)
+
+        x3 = x3 + x4
+
+        x = x * x3
+
+        x = x.view(-1, self.input_channels, 1, 1)
+        return x
+
+
+class ICPA(nn.Module):
+
+    def __init__(self, in_channels, out_channels, channelAttention_reduce=4):
+        super().__init__()
+
+        self.C = in_channels
+        self.O = out_channels
+
+        assert in_channels == out_channels
+        self.ca = ChannelAttention(input_channels=in_channels, internal_neurons=in_channels // channelAttention_reduce)
+        self.dconv5_5 = nn.Conv2d(in_channels, in_channels, kernel_size=5, padding=2, groups=in_channels)
+        self.dconv1_7 = nn.Conv2d(in_channels, in_channels, kernel_size=(1, 7), padding=(0, 3), groups=in_channels)
+        self.dconv7_1 = nn.Conv2d(in_channels, in_channels, kernel_size=(7, 1), padding=(3, 0), groups=in_channels)
+        self.dconv1_11 = nn.Conv2d(in_channels, in_channels, kernel_size=(1, 11), padding=(0, 5), groups=in_channels)
+        self.dconv11_1 = nn.Conv2d(in_channels, in_channels, kernel_size=(11, 1), padding=(5, 0), groups=in_channels)
+        self.dconv1_21 = nn.Conv2d(in_channels, in_channels, kernel_size=(1, 21), padding=(0, 10), groups=in_channels)
+        self.dconv21_1 = nn.Conv2d(in_channels, in_channels, kernel_size=(21, 1), padding=(10, 0), groups=in_channels)
+        self.conv = nn.Conv2d(in_channels, in_channels, kernel_size=(1, 1), padding=0)
+        self.act = nn.GELU()
+
+    def forward(self, inputs, ill):
+        inputs = inputs.permute(0, 3, 1, 2)
+
+        #   Global Perceptron
+        inputs = self.conv(inputs)
+        inputs = self.act(inputs)
+
+        channel_att_vec = self.ca(inputs, ill)
+        inputs = channel_att_vec * inputs
+
+        x_init = self.dconv5_5(inputs)
+        x_1 = self.dconv1_7(x_init)
+        x_1 = self.dconv7_1(x_1)
+        x_2 = self.dconv1_11(x_init)
+        x_2 = self.dconv11_1(x_2)
+        x_3 = self.dconv1_21(x_init)
+        x_3 = self.dconv21_1(x_3)
+        x = x_1 + x_2 + x_3 + x_init
+        spatial_att = self.conv(x)
+        out = spatial_att * inputs
+        out = self.conv(out)
+        out = out.permute(0, 2, 3, 1)
+
+        return out
+
+
+# import cv2
+
+
+def _no_grad_trunc_normal_(tensor, mean, std, a, b):
+    def norm_cdf(x):
+        return (1. + math.erf(x / math.sqrt(2.))) / 2.
+
+    if (mean < a - 2 * std) or (mean > b + 2 * std):
+        warnings.warn("mean is more than 2 std from [a, b] in nn.init.trunc_normal_. "
+                      "The distribution of values may be incorrect.",
+                      stacklevel=2)
+    with torch.no_grad():
+        l = norm_cdf((a - mean) / std)
+        u = norm_cdf((b - mean) / std)
+        tensor.uniform_(2 * l - 1, 2 * u - 1)
+        tensor.erfinv_()
+        tensor.mul_(std * math.sqrt(2.))
+        tensor.add_(mean)
+        tensor.clamp_(min=a, max=b)
+        return tensor
+
+
+def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
+    # type: (Tensor, float, float, float, float) -> Tensor
+    return _no_grad_trunc_normal_(tensor, mean, std, a, b)
+
+
+def variance_scaling_(tensor, scale=1.0, mode='fan_in', distribution='normal'):
+    fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
+    if mode == 'fan_in':
+        denom = fan_in
+    elif mode == 'fan_out':
+        denom = fan_out
+    elif mode == 'fan_avg':
+        denom = (fan_in + fan_out) / 2
+    variance = scale / denom
+    if distribution == "truncated_normal":
+        trunc_normal_(tensor, std=math.sqrt(variance) / .87962566103423978)
+    elif distribution == "normal":
+        tensor.normal_(std=math.sqrt(variance))
+    elif distribution == "uniform":
+        bound = math.sqrt(3 * variance)
+        tensor.uniform_(-bound, bound)
+    else:
+        raise ValueError(f"invalid distribution {distribution}")
+
+
+def lecun_normal_(tensor):
+    variance_scaling_(tensor, mode='fan_in', distribution='truncated_normal')
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x, *args, **kwargs):
+        x = self.norm(x)
+        return self.fn(x, *args, **kwargs)
+
+
+class GELU(nn.Module):
+    def forward(self, x):
+        return F.gelu(x)
+
+
+def conv(in_channels, out_channels, kernel_size, bias=False, padding=1, stride=1):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size // 2), bias=bias, stride=stride)
+
+
+# input [bs,28,256,310]  output [bs, 28, 256, 256]
+def shift_back(inputs, step=2):
+    [bs, nC, row, col] = inputs.shape
+    down_sample = 256 // row
+    step = float(step) / float(down_sample * down_sample)
+    out_col = row
+    for i in range(nC):
+        inputs[:, i, :, :out_col] = \
+            inputs[:, i, :, int(step * i):int(step * i) + out_col]
+    return inputs[:, :, :, :out_col]
+
+
+class Aff_channel(nn.Module):
+    def __init__(self, dim, channel_first=True):
+        super().__init__()
+        # learnable parameters
+        self.alpha = nn.Parameter(torch.ones([1, dim, 1, 1]))
+        self.beta = nn.Parameter(torch.zeros([1, dim, 1, 1]))
+        self.color = nn.Parameter(torch.eye(dim))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        x = x * self.alpha + self.beta
+        x = x.view(b * h * w, c)
+        x = torch.matmul(x, self.color)  # 注意：这里假设self.color可以广播到x
+        x = x.view(b, c, h, w)
+
+        return x
+
+
+class Illumination_Estimator(nn.Module):
+    def __init__(
+            self, n_fea_middle, n_fea_in=4, n_fea_out=3):  # __init__部分是内部属性，而forward的输入才是外部输入
+        super(Illumination_Estimator, self).__init__()
+
+        self.conv1 = nn.Conv2d(n_fea_in, n_fea_middle, kernel_size=1, bias=True)
+
+        self.depth_conv = nn.Conv2d(
+            n_fea_middle, n_fea_middle, kernel_size=5, padding=2, bias=True, groups=n_fea_in)
+
+        self.conv2 = nn.Conv2d(n_fea_middle, n_fea_out, kernel_size=1, bias=True)
+        self.conv0_1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.norm0 = Aff_channel(16)
+        self.conv0_2 = nn.Conv2d(16, 1, kernel_size=3, stride=1, padding=1)
+        self.leaky_relu = nn.LeakyReLU(0.1)
+
+    def forward(self, img):
+        # img:        b,c=3,h,w
+        # mean_c:     b,c=1,h,w
+
+        # illu_fea:   b,c,h,w
+        # illu_map:   b,c=3,h,w
+
+        # mean_c = img.mean(dim=1).unsqueeze(1)
+        mean_c = self.leaky_relu(self.conv0_1(img))
+        mean_c = self.norm0(mean_c)
+        mean_c = self.conv0_2(mean_c)
+        # stx()
+        input = torch.cat([img, mean_c], dim=1)
+
+        x_1 = self.conv1(input)
+        illu_fea = self.depth_conv(x_1)
+        illu_map = self.conv2(illu_fea)
+        return illu_fea, illu_map
+
+
+class IG_MSA(nn.Module):
+    def __init__(
+            self,
+            dim,
+            dim_head=64,
+            heads=8,
+    ):
+        super().__init__()
+        self.num_heads = heads
+        self.dim_head = dim_head
+        self.to_q = nn.Linear(dim, dim_head * heads, bias=False)
+        self.to_k = nn.Linear(dim, dim_head * heads, bias=False)
+        self.to_v = nn.Linear(dim, dim_head * heads, bias=False)
+        self.rescale = nn.Parameter(torch.ones(heads, 1, 1))
+        self.proj = nn.Linear(dim_head * heads, dim, bias=True)
+        self.pos_emb = nn.Sequential(
+            nn.Conv2d(dim, dim, 3, 1, 1, bias=False, groups=dim),
+            GELU(),
+            nn.Conv2d(dim, dim, 3, 1, 1, bias=False, groups=dim),
+        )
+        self.dim = dim
+
+    def forward(self, x_in, illu_fea_trans):
+
+        b, h, w, c = x_in.shape
+        x = x_in.reshape(b, h * w, c)
+        q_inp = self.to_q(x)
+        k_inp = self.to_k(x)
+        v_inp = self.to_v(x)
+        illu_attn = illu_fea_trans  # illu_fea: b,c,h,w -> b,h,w,c
+        q, k, v, illu_attn = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.num_heads),
+                                 (q_inp, k_inp, v_inp, illu_attn.flatten(1, 2)))
+        v = v * illu_attn
+        # q: b,heads,hw,c
+        q = q.transpose(-2, -1)
+        k = k.transpose(-2, -1)
+        v = v.transpose(-2, -1)
+        q = F.normalize(q, dim=-1, p=2)
+        k = F.normalize(k, dim=-1, p=2)
+        attn = (k @ q.transpose(-2, -1))  # A = K^T*Q
+        attn = attn * self.rescale
+        attn = attn.softmax(dim=-1)
+        x = attn @ v  # b,heads,d,hw
+        x = x.permute(0, 3, 1, 2)  # Transpose
+        x = x.reshape(b, h * w, self.num_heads * self.dim_head)
+        out_c = self.proj(x).view(b, h, w, c)
+        out_p = self.pos_emb(v_inp.reshape(b, h, w, c).permute(
+            0, 3, 1, 2)).permute(0, 2, 3, 1)
+        out = out_c + out_p
+
+        return out
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult=4):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(dim, dim * mult, 1, 1, bias=False),
+            GELU(),
+            nn.Conv2d(dim * mult, dim * mult, 3, 1, 1,
+                      bias=False, groups=dim * mult),
+            GELU(),
+            nn.Conv2d(dim * mult, dim, 1, 1, bias=False),
+        )
+
+    def forward(self, x):
+        """
+        x: [b,h,w,c]
+        return out: [b,h,w,c]
+        """
+        out = self.net(x.permute(0, 3, 1, 2).contiguous())
+        return out.permute(0, 2, 3, 1)
+
+
+class IGAB(nn.Module):
+    def __init__(
+            self,
+            dim,
+            dim_head=64,
+            heads=8,
+            num_blocks=2,
+    ):
+        super().__init__()
+        self.blocks = nn.ModuleList([])
+        for _ in range(num_blocks):
+            self.blocks.append(nn.ModuleList([
+                IG_MSA(dim=dim, dim_head=dim_head, heads=heads),
+                PreNorm(dim, FeedForward(dim=dim))
+            ]))
+
+    def forward(self, x, illu_fea):
+        """
+        x: [b,c,h,w]
+        illu_fea: [b,c,h,w]
+        return out: [b,c,h,w]
+        """
+        x = x.permute(0, 2, 3, 1)
+        for (attn, ff) in self.blocks:
+            x = attn(x, illu_fea_trans=illu_fea.permute(0, 2, 3, 1)) + x
+            x = ff(x) + x
+        out = x.permute(0, 3, 1, 2)
+        return out
+
+
+class IGCAB(nn.Module):
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            channelAttention_reduce=4,
+            num_blocks=2,
+    ):
+        super().__init__()
+        self.blocks = nn.ModuleList([])
+        for _ in range(num_blocks):
+            self.blocks.append(nn.ModuleList([
+                ICPA(in_channels=in_channels, out_channels=in_channels),
+                PreNorm(in_channels, FeedForward(dim=in_channels))
+            ]))
+
+    def forward(self, x, ill):
+        """
+        x: [b,c,h,w]
+        illu_fea: [b,c,h,w]
+        return out: [b,c,h,w]
+        """
+        x = x.permute(0, 2, 3, 1)
+        for (attn, ff) in self.blocks:
+            x = attn(x, ill) + x
+
+            x = ff(x) + x
+        out = x.permute(0, 3, 1, 2)
+        return out
+
+
+class Denoiser(nn.Module):
+    def __init__(self, in_dim=3, out_dim=3, dim=31, level=2, num_blocks=[2, 4, 4]):
+        super(Denoiser, self).__init__()
+        self.dim = dim
+        self.level = level
+
+        # Input projection
+        self.embedding = nn.Conv2d(in_dim, self.dim, 3, 1, 1, bias=False)
+
+        # Encoder
+        self.encoder_layers = nn.ModuleList([])
+        dim_level = dim
+        for i in range(level):
+            self.encoder_layers.append(nn.ModuleList([
+                IGCAB(
+                    in_channels=dim_level, out_channels=dim_level, num_blocks=num_blocks[i]
+                ),
+                nn.Conv2d(dim_level, dim_level * 2, 4, 2, 1, bias=False),
+                nn.Conv2d(dim_level, dim_level * 2, 4, 2, 1, bias=False)
+            ]))
+            dim_level *= 2
+
+        # Bottleneck
+        self.bottleneck = IGAB(
+            dim=dim_level, dim_head=dim, heads=dim_level // dim, num_blocks=num_blocks[-1])
+
+        # Decoder
+        self.decoder_layers = nn.ModuleList([])
+        for i in range(level):
+            self.decoder_layers.append(nn.ModuleList([
+                nn.ConvTranspose2d(dim_level, dim_level // 2, stride=2,
+                                   kernel_size=2, padding=0, output_padding=0),
+                nn.Conv2d(dim_level, dim_level // 2, 1, 1, bias=False),
+                IGAB(
+                    dim=dim_level // 2, num_blocks=num_blocks[level - 1 - i], dim_head=dim,
+                    heads=(dim_level // 2) // dim),
+            ]))
+            dim_level //= 2
+
+        # Output projection
+        self.mapping = nn.Conv2d(self.dim, out_dim, 3, 1, 1, bias=False)
+
+        # activation function
+        self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward(self, x, illu_fea):
+        """
+        x:          [b,c,h,w]         x是feature, 不是image
+        illu_fea:   [b,c,h,w]
+        return out: [b,c,h,w]
+        """
+
+        # Embedding
+        fea = self.embedding(x)
+
+        # Encoder
+        fea_encoder = []
+        illu_fea_list = []
+        for (IGCAB, FeaDownSample, IlluFeaDownsample) in self.encoder_layers:
+            fea = IGCAB(fea, illu_fea)  # bchw
+            illu_fea_list.append(illu_fea)
+            fea_encoder.append(fea)
+            fea = FeaDownSample(fea)
+            illu_fea = IlluFeaDownsample(illu_fea)
+
+        # Bottleneck
+        fea = self.bottleneck(fea, illu_fea)
+
+        # Decoder
+        for i, (FeaUpSample, Fution, LeWinBlcok) in enumerate(self.decoder_layers):
+            fea = FeaUpSample(fea)
+            fea = Fution(
+                torch.cat([fea, fea_encoder[self.level - 1 - i]], dim=1))
+            illu_fea = illu_fea_list[self.level - 1 - i]
+            fea = LeWinBlcok(fea, illu_fea)
+
+        # Mapping
+        out = self.mapping(fea) + x
+
+        return out
+
+
+class LIPResNet_Single_Stage(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, n_feat=31, level=2, num_blocks=[1, 1, 1]):
+        super(LIPResNet_Single_Stage, self).__init__()
+        self.estimator = Illumination_Estimator(n_feat)
+        self.denoiser = Denoiser(in_dim=in_channels, out_dim=out_channels, dim=n_feat, level=level,
+                                 num_blocks=num_blocks)  #### 将 Denoiser 改为 img2img
+
+    def forward(self, img):
+        # img:        b,c=3,h,w
+
+        # illu_fea:   b,c,h,w
+        # illu_map:   b,c=3,h,w
+        illu_fea, illu_map = self.estimator(img)
+
+        input_img = img * illu_map + img
+        output_img = self.denoiser(input_img, illu_fea)
+
+        return output_img
+
+
+class LIPResNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, n_feat=31, stage=3, num_blocks=[1, 1, 1]):
+        super(LIPResNet, self).__init__()
+        self.stage = stage
+
+        modules_body = [
+            LIPResNet_Single_Stage(in_channels=in_channels, out_channels=out_channels, n_feat=n_feat, level=2,
+                                       num_blocks=num_blocks)
+            for _ in range(stage)]
+
+        self.body = nn.Sequential(*modules_body)
+
+    def forward(self, x):
+        """
+        x: [b,c,h,w]
+        return out:[b,c,h,w]
+        """
+        out = self.body(x)
+
+        return out
+
+
+if __name__ == '__main__':
+    from fvcore.nn import FlopCountAnalysis
+
+    model = LIPResNet(stage=1, n_feat=40, num_blocks=[1, 2, 2]).cuda()
+    print(model)
+    inputs = torch.randn((1, 3, 256, 256)).cuda()
+    flops = FlopCountAnalysis(model, inputs)
+    n_param = sum([p.nelement() for p in model.parameters()])  # 所有参数数量
+    print(f'GMac:{flops.total() / (1024 * 1024 * 1024)}')
+    print(f'Params:{n_param}')
 
